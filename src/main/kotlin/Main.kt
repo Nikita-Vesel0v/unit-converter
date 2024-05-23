5d094a2e58542e7d50c329ddd122d2ba1bea346d
Nikita-Vesel0v <nikiveselov2004@gmail.com>
2024-05-23 13:10:09 +0300

    added conversion from different units of distance to meters

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,62 @@
 import java.util.*
 
-fun kilometerToMeter(x: Int) = x * 1000
+val unitsMeasure = mapOf(
+    "meter" to listOf("m", "meter", "meters"),
+    "kilometer" to listOf("km", "kilometer", "kilometers"),
+    "centimeter" to listOf("cm", "centimeter", "centimeters"),
+    "millimeter" to listOf("mm", "millimeter", "millimeters"),
+    "mile" to listOf("mi", "mile", "miles"),
+    "yard" to listOf("yd", "yard", "yards"),
+    "feet" to listOf("ft", "foot", "feet"),
+    "inches" to listOf("in", "inch", "inches"),
+)
+
+val coefficientToMeter = mapOf(
+    "meter" to 1.0,
+    "kilometer" to 1000.0,
+    "centimeter" to 0.01,
+    "millimeter" to 0.001,
+    "mile" to 1609.35,
+    "yard" to 0.9144,
+    "feet" to 0.3048,
+    "inches" to 0.0254,
+)
+
+fun toMeter(unit: String, value: Double): Double =
+    if (unit in coefficientToMeter.keys) value * (coefficientToMeter[unit]!!) //use !! because check it above
+    else -1.0
+
+fun addLetterS(unit: String, value: Double) =
+    if (value != 1.0 && (unit !in listOf("feet", "inches"))) "s"
+    else ""
+
+
 
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    print("Enter a number and a measure: ")
+    print("Enter a number and a measure of length: ")
 
-    val distance = scanner.nextInt()
-    val measure = scanner.next()
-
-    if (measure.lowercase() == "km" || measure.lowercase() == "kilometer" || measure.lowercase() == "kilometers") {
-        print(distance)
-        print(if (distance == 1) {
-            " kilometer "
-        } else {
-            " kilometers "
-        })
-        println("is ${kilometerToMeter(distance)} meters")
-    } else {
-        println("Wrong input")
+    val distance = try {
+        scanner.nextDouble()
+    } catch(e: InputMismatchException) {
+        scanner.next().toDouble()
+    }
+    val inputUnit = scanner.next().lowercase()
+    val unit = try {
+        unitsMeasure.filter { inputUnit in it.value }.keys.first()
+    } catch (e: NoSuchElementException) {
+        "unknown unit"
     }
 
-
-
-
+    if (unit != "unknown unit") {
+        val convertedDistance = toMeter(unit, distance)
+        if (convertedDistance == -1.0) {
+            println("We can't convert it to meter")
+        } else {
+            print("$distance $unit${addLetterS(unit, distance)} is $convertedDistance meter${addLetterS("meter", convertedDistance)}")
+        }
+    } else {
+        println("Wrong input. Unknown unit $inputUnit")
+    }
 }
